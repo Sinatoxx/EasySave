e6939d500798cd239ea9f2b7c26e01dfb48f2184
Feres Hidri <fereshidri99@gmail.com>
2026-05-11 20:01:43 +0100

    business app2

--- EasySave/Services/BusinessAppService.cs
+++ EasySave/Services/BusinessAppService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Text;
 using System.Linq;
-
+#modification
 namespace EasySave.Services
 {
     internal class BusinessAppService
