5a6affd5283e417d3a510a0ecbed603c6eed8e96
Feres Hidri <fereshidri99@gmail.com>
2026-05-11 19:37:49 +0100

    crypto service

---
+++ EasySave/Services/CryptoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace EasySave.Services
+{
+    internal class CryptoService
+    {
+    
+        public long Encrypt(string src, string dst)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+            Process p = Process.Start("CryptoSoft.exe", $"\"{src}\" \"{dst}\"");
+            p.WaitForExit();
+            sw.Stop();
+            return p.ExitCode == 0 ? sw.ElapsedMilliseconds : -1;
+        }
+    }
+}
