f30dcc93fc22a6f563f89c6137471beb4e2d7ad2
Feres Hidri <fereshidri99@gmail.com>
2026-05-14 00:32:06 +0100

    changement cryptoservice

--- EasySave/Services/CryptoService.cs
+++ EasySave/Services/CryptoService.cs
@@ -1,16 +1,47 @@
+using System;
 using System.Diagnostics;
-namespace EasySaveApp.Services
+using System.IO;
+
+namespace EasySave.Services
 {
     public class CryptoService
     {
         public long Encrypt(string source, string dest)
         {
+            // On vérifie si l'exécutable CryptoSoft existe
+            // Il doit être dans le même dossier que EasySave.exe
+            string cryptoSoftPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CryptoSoft.exe");
+
+            if (!File.Exists(cryptoSoftPath)) return -1;
+
             Stopwatch sw = Stopwatch.StartNew();
-            // Appel de l'exécutable CryptoSoft
-            Process p = Process.Start("CryptoSoft.exe", $"\"{source}\" \"{dest}\"");
-            p.WaitForExit();
-            sw.Stop();
-            return p.ExitCode == 0 ? sw.ElapsedMilliseconds : -1;
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = cryptoSoftPath,
+                    Arguments = $"\"{source}\" \"{dest}\"", // On met des guillemets pour les espaces
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+
+                using (Process process = Process.Start(startInfo))
+                {
+                    process.WaitForExit(); // On attend que CryptoSoft finisse
+                    sw.Stop();
+
+                    if (process.ExitCode == 0)
+                    {
+                        return sw.ElapsedMilliseconds; // On renvoie le temps > 0
+                    }
+                    return -1; // Code erreur si CryptoSoft a échoué
+                }
+            }
+            catch
+            {
+                return -1;
+            }
         }
     }
 }
